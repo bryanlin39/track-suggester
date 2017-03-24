426f3e8a1aa3cab1b4f9ed375b8d4861718df358
Bryan Lin <bryanlin39@gmail.com>
2017-03-24 13:53:19 -0700

    add variables and assign them the values from the survey form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
-  $("#name").submit(function(event) {
+  // $("#name").submit(function(event) {
+  //   event.preventDefault();
+  //
+  //   var name = $("#name-input").val();
+  //   $("#submitted-name").text(name);
+  //
+  //   $("#heading, #start").fadeOut();
+  //   $("#survey-form").fadeIn();
+  // });
+
+  $("#survey").submit(function(event) {
     event.preventDefault();
-    var name = $("#name-input").val();
-    $("#submitted-name").text(name);
 
+    var creative = $("input:radio[name=creative]:checked").val();
+    var company = $("input:radio[name=company]:checked").val();
+    var devType = $("input:radio[name=dev-type]:checked").val();
+    var games = $("input:radio[name=games]:checked").val();
+    var build = $("input:radio[name=build]:checked").val();
+
+    // $("#survey-form").slideUp();
+    // $("#result").slideDown();
   });
-
 });
